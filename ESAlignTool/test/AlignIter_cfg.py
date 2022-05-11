d1721c0440e3fc31cf797b7519130e053040157e
ywkao <ywkao@hep1.phys.ntu.edu.tw>
2022-05-11 06:05:51 +0200

    Enable report log on every 100 events

--- ESAlignTool/test/AlignIter_cfg.py
+++ ESAlignTool/test/AlignIter_cfg.py
@@ -31,7 +31,8 @@ process.newESAlignment = cms.ESSource("PoolDBESSource",
         )
 process.es_prefer_GlobalPositionDB = cms.ESPrefer("PoolDBESSource", "newESAlignment")
 
-#process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32( 1000 ) # not work
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32( 100 )
 
 # Default Parameter options
 from FWCore.ParameterSet.VarParsing import VarParsing
