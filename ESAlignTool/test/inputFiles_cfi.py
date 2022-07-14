93a5867cf944efe5bc53a13ac086c851015cd036
ywkao <ywkao@hep1.phys.ntu.edu.tw>
2022-07-14 08:31:29 +0200

    Add 2022B EGamma ALCARECO data

--- ESAlignTool/test/inputFiles_cfi.py
+++ ESAlignTool/test/inputFiles_cfi.py
@@ -1,5 +1,66 @@
 FileNames = {
     "default" : [
-        '/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/207/00000/1d603b01-c334-4dfc-9be9-c6e00c155364.root',
+        "/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/207/00000/a7b89716-0f32-4e93-b8eb-411f6c249a74.root", # 9720 
+        #"/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/207/00000/1d603b01-c334-4dfc-9be9-c6e00c155364.root", # 46539
+        #"/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/208/00000/15078f76-e399-45ef-9bc7-f3cb6fa4bd15.root", # 82511 
+        #"/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/208/00000/36aeb8bd-e519-4703-ba1c-b5ef957b8c43.root", # 54356 
+        #"/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/208/00000/3a7ae264-b873-407b-9cf1-e26b5d3770c8.root", # 55320 
+        #"/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/208/00000/5d20fe70-69ff-4d9a-b411-e2e87234ae62.root", # 68503 
+        #"/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/208/00000/88de3fea-582c-4de0-bdd3-226eebd5062b.root", # 77750 
+        #"/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/208/00000/8b212576-55ad-49d9-9b32-34b8eac24265.root", # 71099 
+        #"/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/208/00000/8cd10f57-9782-4791-a567-b2027859c894.root", # 64408 
+        #"/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/208/00000/aab1e6a1-cd7b-41ea-bb73-987e92e40b02.root", # 70695 
+        #"/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/208/00000/ee911dd4-28eb-470f-9a82-c2b2f43f5722.root", # 68491 
+        #"/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/208/00000/fb817839-4c8f-4589-9640-9ee12a8fd5c8.root", # 55298 
+        #"/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/208/00000/fd84ed0a-a472-4b16-936d-e8124d1ebb50.root", # 6143
+    ],
+
+    "batch01" : [
+        "/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/207/00000/1d603b01-c334-4dfc-9be9-c6e00c155364.root", # 46539
+    ],
+
+    "batch02" : [
+        "/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/208/00000/15078f76-e399-45ef-9bc7-f3cb6fa4bd15.root", # 82511 
+    ],
+
+    "batch03" : [
+        "/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/208/00000/36aeb8bd-e519-4703-ba1c-b5ef957b8c43.root", # 54356 
+    ],
+
+    "batch04" : [
+        "/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/208/00000/3a7ae264-b873-407b-9cf1-e26b5d3770c8.root", # 55320 
+    ],
+
+    "batch05" : [
+        "/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/208/00000/5d20fe70-69ff-4d9a-b411-e2e87234ae62.root", # 68503 
+    ],
+
+    "batch06" : [
+        "/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/208/00000/88de3fea-582c-4de0-bdd3-226eebd5062b.root", # 77750 
+    ],
+
+    "batch07" : [
+        "/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/208/00000/8b212576-55ad-49d9-9b32-34b8eac24265.root", # 71099 
+    ],
+
+    "batch08" : [
+        "/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/208/00000/8cd10f57-9782-4791-a567-b2027859c894.root", # 64408 
+    ],
+
+    "batch09" : [
+        "/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/208/00000/aab1e6a1-cd7b-41ea-bb73-987e92e40b02.root", # 70695 
+    ],
+
+    "batch10" : [
+        "/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/208/00000/ee911dd4-28eb-470f-9a82-c2b2f43f5722.root", # 68491 
+    ],
+
+    "batch11" : [
+        "/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/208/00000/fb817839-4c8f-4589-9640-9ee12a8fd5c8.root", # 55298 
+    ],
+
+    "batch12" : [
+        "/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/208/00000/fd84ed0a-a472-4b16-936d-e8124d1ebb50.root", # 6143
+        "/store/data/Run2022B/EGamma/ALCARECO/EcalESAlign-PromptReco-v1/000/355/207/00000/a7b89716-0f32-4e93-b8eb-411f6c249a74.root", # 9720 
     ],
 }
